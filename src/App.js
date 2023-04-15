f80be69da7df1887f13e37d8cb0843f79ddc0dcc
vipevel <vipinsharmaofficial07@gmail.com>
2023-04-15 15:32:02 +0530

    new project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './assets/scss/index.scss';
+import Layout from './services/layout'
+
+function App() {
+  return (
+   <>
+   <Layout>
+
+   </Layout>
+   </>
+  );
+}
+
+export default App;
